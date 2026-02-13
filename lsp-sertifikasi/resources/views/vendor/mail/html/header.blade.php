39b33fba1c9b87d711646f3d61280e29723ab96f
FARHAN MUZHAFFAR TIRAS PUTRA <96176429+Hanhan73@users.noreply.github.com>
2026-02-13 23:56:07 +0700

    Update header.blade.php

--- lsp-sertifikasi/resources/views/vendor/mail/html/header.blade.php
+++ lsp-sertifikasi/resources/views/vendor/mail/html/header.blade.php
@@ -1,12 +1,12 @@
 @props(['url'])
 <tr>
-<td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="{{ asset('images/logo-lsp.png') }}" class="logo" alt="Laravel Logo">
-@else
-{!! $slot !!}
-@endif
-</a>
-</td>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
+            @if (trim($slot) === 'Laravel')
+            <img src="{{ asset('images/logo-lsp.png') }}" class="logo" alt="Laravel Logo" style="width: fit-content;">
+            @else
+            {!! $slot !!}
+            @endif
+        </a>
+    </td>
 </tr>
